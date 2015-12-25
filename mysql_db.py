597086b4bde70444b69b613a92c09abcc6e5f48f
root <root@localhost.localdomain>
2015-12-24 23:35:48 -0500

    mysqldb.py implemented

--- mysql_db.py
+++ mysql_db.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
+#!/projects/contactcars/ven/bin/python2.7
 import MySQLdb
 import time
-
+import csv
 class Db:
-    def __init__(self, host, user, passwd, db ):
-        self.db = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db)
+    def __init__(self, host, user, passwd, db ,charset):
+        self.db = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db, charset=charset)
         self.db.set_character_set('utf8')
         self.cur = self.db.cursor(MySQLdb.cursors.DictCursor)
         self.cur.execute('SET NAMES utf8;')
@@ -29,10 +30,10 @@ class Db:
             print "exist"
             return False
         query = '''INSERT INTO 26March2015
-                   ( _widgetName,_source,_resultNumber,_pageUrl,make,model,model_year,price,cc,km,`desc`,addedon)
+                   ( _widgetName,_source,_resultNumber,_pageUrl,make,model,model_year,price,cc,km,`descr`,addedon)
                    VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'''
         self.cur.execute(query, ('contactcaes',_source,'0',data['href'],data['maker'], data['model'],data['year'],
-                                data['price'],data['cc'],data['km'],data['desc'],''))
+                                data['price'],data['cc'],data['km'],data['descr'],''))
         self.db.commit()
         return True
 
@@ -60,17 +61,25 @@ class Db:
         self.db.commit()
 
 
-if __name__ == '__main__':
-    print "hi there!!"
-    db = Db(host='localhost',user='root',passwd='',db='pee')
-    data = {}
-    data['href'] = 'nn'
-    data['desc'] = 'بحالة ودهان المصنع وصيانات في التوكيل'
-    data['cc'] = '00'
-    data['km'] = '00'
-    data['price'] = '00'
-    data['year'] = '22'
-    data['model'] = 'mm'
-    data['maker'] = 'mk'
-    db.insert_post(data)
+# if __name__ == '__main__':
+#     print "hi there!!"
+#     db = Db(host='localhost',user='root',passwd='123456',db='Hamouda', charset='utf8')
+#     mf = open('carsdata.csv')
+#     lines = csv.DictReader(mf)    
+#     for data in lines:
+#         # data = {}
+#         # data['href'] = 'nn'
+#         data['descr'] = 'بحالة ودهان المصنع وصيانات في التوكيل'
+#         data['cc'] = '00'
+#         data['km'] = '00'
+#         # data['price'] = '00'
+#         # data['year'] = '22'
+#         # data['model'] = 'mm'
+#         # data['maker'] = 'mk'
+#         print data['descr']        
+#         break
+#         for k,v in data.iteritems():
+#             data[k] = data[k].encode('utf-8')
+
+#         db.insert_post(data)
 
--- script.py
+++ script.py
@@ -1,4 +1,32 @@
-#!$HOME/contactcars/ven/bin/python
+# -*- coding: utf-8 -*-
+#!/usr/local/bin/python2.7
 from mysql_db import *
+DATABASE_SETTINGS = dict(
+    host='HHamouda.mysql.pythonanywhere-services.com',
+    # port = 3306,
+    user='HHamouda',
+    passwd='mysql',
+    db='test', 
+    charset='utf8'
+)
+    
 
 
+
+if __name__ == '__main__':    
+    print DATABASE_SETTINGS
+    try:
+        db = Db(**DATABASE_SETTINGS)
+        print db
+        mf = open('carsdata.csv')
+        lines = csv.DictReader(mf)    
+        for data in lines:                        
+            data['cc'] = '00'
+            data['km'] = '00'            
+            print data['descr']                                
+            db.insert_post(data)
+        except MySQLdb.Error,e:
+        print "exception"
+        print "Mysql Error %d: %s" % (e.args[0], e.args[1])
+
+    
